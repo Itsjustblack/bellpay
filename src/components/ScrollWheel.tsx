6cd34907d3f3efe165a10d837945ba44e7d7206a
jason aghedo <jason.aghedo@gmail.com>
2024-12-12 08:28:11 +0100

    fix: image carousel

--- src/components/ScrollWheel.tsx
+++ src/components/ScrollWheel.tsx
@@ -1,45 +1,35 @@
-import Visa from "@icons/visaLogo.svg";
 import MasterCard from "@icons/mastercardLogo.svg";
 import Paypal from "@icons/paypalLogo.svg";
 import Verve from "@icons/verveLogo.svg";
+import Visa from "@icons/visaLogo.svg";
 
 import { FC } from "react";
+
+const images = [Visa, MasterCard, Paypal, Verve];
 
 const ScrollWheel: FC = () => {
 	return (
 		<div className="w-full overflow-hidden bg-[#790096] px-[35px] py-3">
 			<div
 				style={{ zIndex: 20 }}
-				className="carousel relative grid h-[81px] w-full grid-cols-4 items-center space-x-5"
+				className="carousel relative flex h-[81px] w-full items-center space-x-[80px]"
 			>
-				<div className="flex max-h-[40px] flex-1">
+				{images.map((image) => (
 					<img
-						className="h-auto w-full object-contain"
-						src={Visa}
+						key={image}
+						className="h-[40px]"
+						src={image}
 						alt=""
 					/>
-				</div>
-				<div className="flex max-h-[40px] flex-1">
+				))}
+				{images.map((image) => (
 					<img
-						className="h-auto w-full object-contain"
-						src={MasterCard}
+						key={image}
+						className="h-[40px]"
+						src={image}
 						alt=""
 					/>
-				</div>
-				<div className="flex max-h-[40px] flex-1">
-					<img
-						className="h-auto w-full object-contain"
-						src={Paypal}
-						alt=""
-					/>
-				</div>
-				<div className="flex max-h-[40px] flex-1">
-					<img
-						className="h-auto w-full object-contain"
-						src={Verve}
-						alt=""
-					/>
-				</div>
+				))}
 			</div>
 		</div>
 	);
