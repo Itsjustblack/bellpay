9a420bb15ffd4ec09a89e9a84b207b39e8ec3819
jason aghedo <jason.aghedo@gmail.com>
2024-12-12 14:40:40 +0100

    fix: final commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
-import * as path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@sections": path.resolve(__dirname, "./src/sections"),
-			"@pages": path.resolve(__dirname, "./src/pages"),
-			"@icons": path.resolve(__dirname, "./src/assets/icons"),
-			"@images": path.resolve(__dirname, "./src/assets/images"),
-		},
-	},
-	assetsInclude: ["**/*.svg"], // Include SVGs as static assets
+	plugins: [tsconfigPaths(), react()],
 });
